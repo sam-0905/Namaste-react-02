7605bb04fc789361afaeb0b371a8c9100fc8516d
AllanSam <allansam0905@gmail.com>
2023-01-11 14:32:51 +0530

    updated docs

---
+++ Coding/index.js
@@ -0,0 +1,33 @@
+const heading = React.createElement("h1", {
+        id: "title"
+    },
+    "Hello World !"
+)
+
+
+const heading1 = React.createElement("h2", {
+        id: "title",
+        //style: { color: "red" },
+    },
+    "Learing React from zero to Hero!"
+)
+
+const heading2 = React.createElement("h3", {
+        id: "title"
+    },
+    "From Akshay"
+)
+
+const container = React.createElement("div", {
+    id: "container",
+    className: "class"
+}, [heading, heading1, heading2])
+
+
+console.log(heading1);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+root.render(container);
